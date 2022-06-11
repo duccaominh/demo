01c8c131e0bdf0af09ed27c931aeb36636c91fff
duccaominh <duccaom@nashtechglobal.com>
2022-06-11 21:53:57 +0700

    feat: demo skeleton

---
+++ src/Sport.java
@@ -0,0 +1,24 @@
+/**
+ *
+ * @author duccaom
+ * @version 1.0
+ * @since 2022/06/11
+ */
+public class Sport {
+  private String name;
+
+  public Sport() {
+  }
+
+  public Sport(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
---
+++ src/Student.java
@@ -0,0 +1,55 @@
+/**
+ * @author duccaom
+ * @version 1.0
+ * @since 2022/06/11
+ */
+public class Student {
+
+  private String name;
+  private String email;
+  private String address;
+
+  private Sport sport;
+
+  public Student() {
+  }
+
+  public Student(String name, String email, String address, Sport sport) {
+    this.name = name;
+    this.email = email;
+    this.address = address;
+    this.sport = sport;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public Sport getSport() {
+    return sport;
+  }
+
+  public void setSport(Sport sport) {
+    this.sport = sport;
+  }
+}
